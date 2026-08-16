7b0b488249668178d7291915799f3dd2542e07a1
roshiend <roshiend@yahoo.com>
2026-08-16 09:37:14 +0100

    Use fidelity v2 in Hugging Face Space

--- hf_space/pipeline/__init__.py
+++ hf_space/pipeline/__init__.py
@@ -1,21 +1,29 @@
-"""Pose & Cloth Swap pipeline helpers for Colab / local use."""
+"""Pose & outfit transfer helpers for Hugging Face Spaces."""
 
 from .body_lock import (
     align_pose_donor_to_base_body,
     match_result_body_to_base,
     soft_preserve_torso,
 )
-from .face_lock import lock_face_identity
+from .face_lock import face_identity_similarity, lock_face_identity
+from .fidelity_v2 import FidelityPoseClothPipeline
 from .garment_extract import extract_garment_from_person
 from .memory import free_vram
-from .leffa_sequential import PoseClothPipeline
+from .pose_geometry import assess_pose_pair, retarget_densepose_control, retarget_skeleton
+
+PoseClothPipeline = FidelityPoseClothPipeline
 
 __all__ = [
     "PoseClothPipeline",
+    "FidelityPoseClothPipeline",
     "lock_face_identity",
+    "face_identity_similarity",
     "extract_garment_from_person",
     "align_pose_donor_to_base_body",
     "match_result_body_to_base",
     "soft_preserve_torso",
+    "assess_pose_pair",
+    "retarget_skeleton",
+    "retarget_densepose_control",
     "free_vram",
 ]
